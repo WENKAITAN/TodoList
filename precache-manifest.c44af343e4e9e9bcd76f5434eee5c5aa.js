96999e24d580ab44911418330a3a11eace013184
WenkaiTan <k9162477@icloud.com>
2019-10-04 08:42:15 +0800

    first commit

---
+++ precache-manifest.c44af343e4e9e9bcd76f5434eee5c5aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d2f3f1de056a645f953d1776d035daa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aae35a9f4a6435402b1f",
+    "url": "/static/css/main.76e9534a.chunk.css"
+  },
+  {
+    "revision": "8f95d4832f608e470d80",
+    "url": "/static/js/2.5d6dda6e.chunk.js"
+  },
+  {
+    "revision": "aae35a9f4a6435402b1f",
+    "url": "/static/js/main.480684ae.chunk.js"
+  },
+  {
+    "revision": "e3831bc56b6faa02b209",
+    "url": "/static/js/runtime-main.f949f7e3.js"
+  }
+]);
